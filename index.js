2eecbc226985b6f4cf72a46c50585909b4e84ca1
Pourab Karchaudhuri <36023@Hexaware.com>
2019-01-30 10:23:59 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var request = require('request')
+var express = require('express')
+var bodyParser = require('body-parser');
+ 
+const app = express()
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.get("/",(req,res)=>{
+ 
+    res.send("Server is running successfully. Use /guidewire endpoint")
+ 
+})
+ 
+app.post('/guidewire', (req, res) => {
+ 
+    console.log(req.body.VehModel)
+    // res.send(req.body)
+ 
+    var options = {
+        method: 'POST',
+        url: 'http://18.224.142.110:8280/pc/service/edge/quote/quote',
+        headers:
+        {
+            'cache-control': 'no-cache',
+            'Authorization': 'Basic c3U6Z3c=',
+            'Content-Type': 'application/json'
+        },
+        body:
+        {
+            jsonrpc: '2.0',
+            method: 'quickQuote_Ext',
+            params:
+                [{
+                    DriverName: 'Name',
+                    DriverAge: '34',
+                    VehModel: req.body.VehModel,
+                    VehMake: req.body.VehMake,
+                    NumClaims: req.body.NumClaims,
+                    NumConvictions: req.body.NumConvictions,
+                    yearOfManufacture: req.body.yearOfManufacture,
+                    VehicleCost: req.body.VehicleCost
+                }]
+        },
+        json: true
+    };
+ 
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+ 
+        console.log(body);
+        res.send(body)
+    });
+ 
+})
+ 
+app.listen(8000,()=>{
+    console.log("app listening on 8000")
+})
